760103b0ebb52781e61c9925c499b0f8478e8f81
t-tokio <tt.tamura0913@gmail.com>
2022-03-15 04:02:10 +0900

    データ保存とActiveHashの導入と設定

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,6 +1,6 @@
 class ApplicationController < ActionController::Base
   before_action :basic_auth
-
+  before_action :configure_permitted_parameters, if: :devise_controller?
 
   private
 
@@ -9,4 +9,11 @@ class ApplicationController < ActionController::Base
       username == ENV["BASIC_AUTH_USER_2"] && password == ENV["BASIC_AUTH_PASSWORD_2"]
     end
   end
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.permit(:sign_up,
+                                      keys: [:nickname, :last_name, :first_name, :last_name_kana,
+                                             :first_name_kana, :birthday, :sex_id, :existence_id, :food_allergy])
+  end
+
 end
---
+++ app/models/existence.rb
@@ -0,0 +1,11 @@
+class Existence < ActiveHash::Base
+  self.data = [
+    { id: 1, name: '--' },
+    { id: 2, name: '食物アレルギー有り' },
+    { id: 3, name: '食物アレルギー無し' }
+  ]
+
+  include ActiveHash::Associations
+  has_many :users
+
+end
