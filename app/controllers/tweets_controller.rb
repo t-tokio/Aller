7ec1b2d78bf4c633dff5bad1e35b0bf80330eadf
t-tokio <tt.tamura0913@gmail.com>
2022-03-17 02:27:35 +0900

    新規投稿機能の実装

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,4 +1,33 @@
 class TweetsController < ApplicationController
+  before_action :authenticate_user!, except: [:index]
+
   def index
+   @tweets = Tweet.includes(:user)
+  end
+
+  def new
+   @tweet = Tweet.new
+  end
+
+  def create
+    @tweet = Tweet.new(tweet_params)
+    if @tweet.save
+      redirect_to root_path(@tweet)
+    else
+      render :new
+    end
+  end
+
+  def show
+    @tweet = Tweet.find(params[:id])
+    #@comment = Comment.new
+    #@comments = @tweet.comments.includes(:user)
+  end
+
+
+  private
+
+  def tweet_params
+    params.require(:tweet).permit(:image, :title, :text).merge(user_id: current_user.id)
   end
 end
