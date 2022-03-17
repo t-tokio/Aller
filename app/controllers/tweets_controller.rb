aeae484770832ef0525abe23cb035ddaab8fee12
t-tokio <tt.tamura0913@gmail.com>
2022-03-17 12:51:45 +0900

    つぶやき編集機能の作成

--- app/controllers/tweets_controller.rb
+++ app/controllers/tweets_controller.rb
@@ -1,5 +1,7 @@
 class TweetsController < ApplicationController
-  before_action :authenticate_user!, except: [:index]
+  before_action :authenticate_user!, except: [:show, :index]
+  before_action :set_tweet, only: [:edit, :show, :update, :destroy]
+  before_action :move_to_index, only: [:edit]
 
   def index
    @tweets = Tweet.includes(:user)
@@ -19,15 +21,35 @@ class TweetsController < ApplicationController
   end
 
   def show
-    @tweet = Tweet.find(params[:id])
     #@comment = Comment.new
     #@comments = @tweet.comments.includes(:user)
   end
 
+  def edit
+
+  end
+
+  def update
+    if @tweet.update(tweet_params)
+      redirect_to tweet_path
+    else
+      render :edit
+    end
+  end
 
   private
 
   def tweet_params
     params.require(:tweet).permit(:image, :title, :text).merge(user_id: current_user.id)
+  end
+
+  def set_tweet
+    @tweet = Tweet.find(params[:id])
+  end
+
+  def move_to_index
+    @tweet = Tweet.find(params[:id])
+    @user = @tweet.user
+    redirect_to(root_path) unless @user == current_user
   end
 end
