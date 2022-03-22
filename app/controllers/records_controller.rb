b99bb916f20b4c95a82329e8b4f630704eb88b9b
t-tokio <tt.tamura0913@gmail.com>
2022-03-22 16:02:21 +0900

    新規記録ページの実装

--- app/controllers/records_controller.rb
+++ app/controllers/records_controller.rb
@@ -4,6 +4,23 @@ class RecordsController < ApplicationController
   end
 
   def new
-
+    @record = Record.new
   end
+
+  def create
+    @record = Record.new(record_params)
+    if @record.save
+      redirect_to root_path(@record)
+    else
+      render :new
+    end
+  end
+
+
+  private
+
+  def record_params
+    params.require(:record).permit(:datetime, :age_id, :food, :quantity, :unit_id, :state_id, :note).merge(user_id: current_user.id)
+  end
+
 end
